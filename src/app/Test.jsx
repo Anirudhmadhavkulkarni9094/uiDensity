615bb939de9a0c236b2566ef1b2b0354529cacfd
Anirudhmadhavkulkarni9094 <Anirudhkulkarni9094@gmail.com>
2023-10-26 13:54:27 +0530

    test fix

--- src/app/Test.jsx
+++ src/app/Test.jsx
@@ -8,7 +8,7 @@ function Test() {
         
         <h1 className='text-xl font-semibold m-5'>We take privacy seriously</h1>
         <h1 className='text-3xl font-bold m-5'>Before you get started</h1>
-        <h1 className='text-xl font-semibold text-gray-600 w-1/2 m-auto'>&#34; We won't share your answers with anyone (and won't ever tell you which friends said what about you) &#34;</h1>
+        <h1 className='text-xl font-semibold text-gray-600 w-1/2 m-auto'>&#34; We won &#39; t share your answers with anyone (and won&#39;t ever tell you which friends said what about you) &#34;</h1>
         <h2 className='font-bold'>With love <span className='text-3xl font-cursive'>Jerum Ahod</span></h2>
         <motion.button className='px-5 py-2 bg-black mt-10 text-white text-xl rounded-xl'
             initial = {{scale : 0}}
